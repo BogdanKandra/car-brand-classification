837c04286016fbf96d9911f65ffecc9bb1e2d1b4
BogdanKandra <bogdan.kandra@gmail.com>
2020-09-18 16:28:30 +0300

    Updated comments

--- data_acquisition.py
+++ data_acquisition.py
@@ -10,12 +10,15 @@ import shutil
 ORIGINAL_DATASET_LOCATION = os.path.join(os.path.dirname(os.getcwd()), 'Data', 'Cars')
 REORGANIZED_DATASET_LOCATION = 'dataset'
 
-# Reorganize the original dataset into directories named after the car brands, containing all images representing a car brand, renamed and indexed by their model and year
+# Reorganize the original dataset into directories named after the car brands,
+# containing all images representing a car brand, renamed and indexed by their
+# model and year
 is_dir_predicate = lambda path: os.path.isdir(os.path.join(ORIGINAL_DATASET_LOCATION, path))
 dataset_directories = list(filter(is_dir_predicate, os.listdir(ORIGINAL_DATASET_LOCATION)))
 car_brands = set([directory.split('_')[0] for directory in dataset_directories])
 
-# For each car brand, collect all images representing that brand, rename them appropriately and copy them to a directory in the new location
+# For each car brand, collect all images representing that brand,
+# rename them appropriately and copy them to a directory in the new location
 for brand in car_brands:
     print('>>> Processing brand: {}...'.format(brand))
     brand_directories = [directory for directory in dataset_directories if directory.startswith(brand)]
