fcb248fc60dd0844e89ff56a3564a4270e203120
BogdanKandra <bogdan.kandra@gmail.com>
2020-09-22 12:06:37 +0300

    Implemented function for displaying bar plot without saving it

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 FIGURES_LOCATION = 'figures'
 
 def save_bar_plot(title, xlabel, ylabel, xdata, ydata, color='r', plot_name='figure'):
-    ''' Generates a bar plot using the given data '''
+    ''' Generates a bar plot using the given data and saves it to disk '''
     plt.figure(0, figsize=(19.2, 10.8))
     plt.title(title)
     plt.xlabel(xlabel, fontweight='bold')
@@ -18,3 +18,12 @@ def save_bar_plot(title, xlabel, ylabel, xdata, ydata, color='r', plot_name='fig
     plt.bar(xdata, ydata, color=color)
     plt.savefig(os.path.join(FIGURES_LOCATION, plot_name + '.png'), quality=100)
     plt.close()
+
+def show_bar_plot(figure_index, title, xlabel, ylabel, xdata, ydata, color='r'):
+    ''' Generates a bar plot using the given data and displays it '''
+    plt.figure(figure_index, figsize=(19.2, 10.8))
+    plt.title(title)
+    plt.xlabel(xlabel, fontweight='bold')
+    plt.ylabel(ylabel, fontweight='bold')
+    plt.bar(xdata, ydata, color=color)
+    plt.show()
