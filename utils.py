4ef9fe83325dd66d1cfd10b5c639bae1a43f8e0b
BogdanKandra <bogdan.kandra@gmail.com>
2020-12-03 11:29:42 +0200

    Added augmented data directories and checks for existence of figures and texts directories

--- utils.py
+++ utils.py
@@ -17,6 +17,9 @@ TEXTS_LOCATION = 'texts'
 TRAINING_DIR = 'training_data'
 TEST_SET_LOCATION = os.path.join(TRAINING_DIR, 'test')
 TRAIN_SET_LOCATION = os.path.join(TRAINING_DIR, 'train')
+AUGMENTED_DIR = 'augmented_data'
+TEST_AUGMENT_LOCATION = os.path.join(AUGMENTED_DIR, 'test')
+TRAIN_AUGMENT_LOCATION = os.path.join(AUGMENTED_DIR, 'train')
 TOP10_BRANDS_COUNTS = 'top_10_brands_samples_counts.txt'
 TOP10_BRANDS_INFORMATION = 'top_10_brands_samples_information.txt'
 RESIZE_HEIGHT = 224
@@ -30,6 +33,8 @@ def save_bar_plot(title, xlabel, ylabel, xdata, ydata, color='r', plot_name='fig
     plt.xlabel(xlabel, fontweight='bold')
     plt.ylabel(ylabel, fontweight='bold')
     plt.bar(xdata, ydata, color=color)
+    if os.path.isdir(FIGURES_LOCATION) is False:
+        os.mkdir(FIGURES_LOCATION)
     plt.savefig(os.path.join(FIGURES_LOCATION, plot_name + '.png'), quality=100)
     plt.close()
 
@@ -44,6 +49,8 @@ def show_bar_plot(figure_index, title, xlabel, ylabel, xdata, ydata, color='r'):
 
 def write_dictionary(dictionary, file_name):
     ''' Writes a dictionary to the specified file, as an indented JSON '''
+    if os.path.isdir(TEXTS_LOCATION) is False:
+        os.mkdir(TEXTS_LOCATION)
     with open(os.path.join(TEXTS_LOCATION, file_name), 'w') as f:
         f.write(json.dumps(dictionary, indent=4))
 
